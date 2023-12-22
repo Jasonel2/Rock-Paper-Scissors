812232f0ed1149fa6e603631da27f912d7c1197f
Jason <Jasonel2@uci.edu>
2023-12-22 14:05:31 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    let random = Math.floor(Math.random() * 3);
+    if (random == 0) 
+        return "Rock";
+    else if (random == 1)
+        return "Paper";
+    else
+        return "Scissors";
+}
+
