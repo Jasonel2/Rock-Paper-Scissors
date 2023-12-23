6eee9920de1e3253269b6e0b988b4a56e7279097
Jason <Jasonel2@uci.edu>
2023-12-22 19:27:34 -0800

    final commit

--- index.js
+++ index.js
@@ -8,3 +8,32 @@ function getComputerChoice() {
         return "Scissors";
 }
 
+function playRPS(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+        return  "tie";
+    }
+    else if (playerSelection.toLowerCase() == "rock" && computerSelection == "Paper") {
+        return "You Lose! Paper beats rock"
+    }
+    else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Scissors") {
+        return "You Lose! Scissors beats paper"
+    }
+    else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Rock") {
+        return "You Lose! Rock beats scissors"
+    }
+    else {
+        console.log(playerSelection, computerSelection)
+        return "You win!"
+    }
+}
+
+
+
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Rock Paper Scissors?");
+        const computerSelection = getComputerChoice();
+        console.log(playRPS(playerSelection, computerSelection));
+    }
+}
